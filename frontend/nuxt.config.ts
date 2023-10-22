fa7e06823b11285f769ec481f59c861bc238d7a2
Ahmet <dede_dede_ahmet@hotmail.com>
2023-10-23 01:09:12 +0300

    #dev5 Tailwind has been implemented and configured

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-      '@pinia/nuxt'
-  ]
+    devtools: {enabled: true},
+    modules: [
+        '@pinia/nuxt',
+        '@nuxtjs/tailwindcss'
+    ],
+    tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+        configPath: 'tailwind.config.ts',
+    }
 })
