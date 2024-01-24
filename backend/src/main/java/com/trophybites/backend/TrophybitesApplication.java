e8a760d81900ba77a586707725eec221f635d06d
Ahmet <dede_dede_ahmet@hotmail.com>
2024-01-25 01:28:32 +0300

    #dev42 Initial commit for backend

---
+++ backend/src/main/java/com/trophybites/backend/TrophybitesApplication.java
@@ -0,0 +1,13 @@
+package com.trophybites.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrophybitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrophybitesApplication.class, args);
+	}
+
+}
