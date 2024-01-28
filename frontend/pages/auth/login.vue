696bad9621468ce98cb3b5cb89cf17c0be576293
Ahmet <dede_dede_ahmet@hotmail.com>
2024-01-28 22:33:37 +0300

    #dev36. Login and Register page design

--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
+// import { CredentialResponse } from 'vue3-google-signin'
+
 definePageMeta({
   layout: 'auth'
 })
 
 const axios = useNuxtApp().$axios
+const i18n = useI18n()
 
 const userName = ref<string>('')
 const password = ref<string>('')
 
-const loginDisabled = ref(false)
-const loginLoading = ref(false)
+const loginLoading = ref<boolean>(false)
 
 const login = async () => {
   loginLoading.value = true
@@ -17,42 +19,88 @@ const login = async () => {
   loginLoading.value = false
 }
 
+const user = ref()
+const handleLoginSuccess = async (response: CredentialResponse) => {
+  const { credential } = response
+
+  if (credential) {
+    user.value = await useFetch('/api/google-login', {
+      method: 'POST',
+      body: {
+        token: credential
+      }
+    })
+  }
+  console.log('user', user)
+}
+
+const handleLoginError = () => {
+  console.error('Login failed')
+}
+
+
 </script>
 
 <template>
-  <div class="p-8">
-    <div class="text-center my-24">
-      <h1 class="font-bold">
-        LOGIN TO YOUR ACCOUNT
-      </h1>
-    </div>
-    <form class="px-16">
+  <div class="flex flex-col items-center p-8 mb-36 h-full w-[100vw] sm:w-[420px]">
+    <form class="w-full px-2 sm:px-8 py-8 sm:border-[1px] border-essence dark:border-secondary rounded-md">
       <div class="container">
-        <div class="row">
-          <div class="col">
+        <div class="flex justify-center">
+          <AppLogo
+            :width="128"
+            :redirect="''"
+          />
+        </div>
+        <div class="text-center mt-4">
+          <h1 class="font-bold text-light-text dark:text-dark-text">
+            {{ $t('auth.login-page.message') }}
+          </h1>
+        </div>
+        <div class="mt-8">
+          <div>
             <TextField
               @update:model-value="newValue => userName = newValue"
               :model-value="userName"
-              label="auth.login-page.user-name"
+              label="auth.common.user-name"
             />
           </div>
-          <div class="col">
+          <div>
             <TextField
               @update:model-value="newValue => password = newValue"
               :model-value="password"
-              label="auth.login-page.password"
+              label="auth.common.password"
             />
           </div>
-          <div class="col">
+          <div class="mt-8">
             <ActionButton
               @action="login"
-              label="auth.login"
-              :disabled="loginDisabled"
+              label="auth.common.login"
               :loading="loginLoading"
+              class="w-full"
+            />
+          </div>
+          <div class="text-center flex flex-row mt-4">
+            <span class="border-b-[1px] w-[37.5%] border-secondary mb-3" />
+            <span class="w-[25%] text-light-text dark:text-dark-text">{{ $t('auth.common.or') }}</span>
+            <span class="border-b-[1px] w-[37.5%] border-secondary mb-3" />
+          </div>
+          <div class="text-center mt-4">
+            <GoogleSignInButton
+              @success="handleLoginSuccess"
+              @error="handleLoginError"
+              :locale="i18n.locale.value"
             />
           </div>
         </div>
       </div>
     </form>
+    <div class="text-center w-full py-4 sm:border-[1px] border-essence dark:border-secondary rounded-md mt-12 text-light-text dark:text-dark-text">
+      {{ $t('auth.login-page.register-message') }}
+      <span class="text-secondary font-bold">
+        <NuxtLink to="/auth/register">
+          {{ $t('auth.common.register') }}
+        </NuxtLink>
+      </span>
+    </div>
   </div>
 </template>
--- frontend/pages/auth/register.vue
+++ frontend/pages/auth/register.vue
@@ -1,15 +1,120 @@
 <script setup lang="ts">
-
 definePageMeta({
   layout: 'auth'
 })
 
+const i18n = useI18n()
+
+const fullName = ref<string>('')
+const email = ref<string>('')
+const userName = ref<string>('')
+const password = ref<string>('')
+
+const registerLoading = ref<boolean>(false)
+
+const register = () => {
+  registerLoading.value = true
+  // register
+  registerLoading.value = false
+}
+
+const user = ref()
+const handleLoginSuccess = async (response: CredentialResponse) => {
+  const { credential } = response
+
+  if (credential) {
+    user.value = await useFetch('/api/google-login', {
+      method: 'POST',
+      body: {
+        token: credential
+      }
+    })
+  }
+  console.log('user', user)
+}
+
+const handleLoginError = () => {
+  console.error('Login failed')
+}
+
 </script>
 
 <template>
-  <div>
-    <NuxtLink to="/auth/login">
-      <button> Login </button>
-    </NuxtLink>
+  <div class="flex flex-col items-center p-8 mb-36 h-full w-[100vw] sm:w-[420px]">
+    <form class="w-full px-2 sm:px-8 py-8 sm:border-[1px] border-essence dark:border-secondary rounded-md">
+      <div class="container">
+        <div class="flex justify-center">
+          <AppLogo
+            :width="128"
+            :redirect="''"
+          />
+        </div>
+        <div class="text-center mt-4">
+          <h1 class="font-bold text-light-text dark:text-dark-text">
+            {{ $t('auth.register-page.message') }}
+          </h1>
+        </div>
+        <div class="mt-8">
+          <div>
+            <TextField
+              @update:model-value="newValue => fullName = newValue"
+              :model-value="fullName"
+              label="auth.common.full-name"
+            />
+          </div>
+          <div>
+            <TextField
+              @update:model-value="newValue => email = newValue"
+              :model-value="email"
+              label="auth.common.email"
+            />
+          </div>
+          <div>
+            <TextField
+              @update:model-value="newValue => userName = newValue"
+              :model-value="userName"
+              label="auth.common.user-name"
+            />
+          </div>
+          <div>
+            <TextField
+              @update:model-value="newValue => password = newValue"
+              :model-value="password"
+              label="auth.common.password"
+              :append-inner-icon="{name: 'bi:eye', color: ''}"
+              input-type="password"
+            />
+          </div>
+          <div class="mt-8">
+            <ActionButton
+              @action="register"
+              label="auth.common.register"
+              :loading="registerLoading"
+              class="w-full"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="text-center flex flex-row mt-4">
+        <span class="border-b-[1px] w-[37.5%] border-secondary mb-3" />
+        <span class="w-[25%] text-light-text dark:text-dark-text">{{ $t('auth.common.or') }}</span>
+        <span class="border-b-[1px] w-[37.5%] border-secondary mb-3" />
+      </div>
+      <div class="text-center mt-4">
+        <GoogleSignInButton
+          @success="handleLoginSuccess"
+          @error="handleLoginError"
+          :locale="i18n.locale.value"
+        />
+      </div>
+    </form>
+    <div class="text-center w-full py-4 sm:border-[1px] border-essence dark:border-secondary rounded-md mt-12 text-light-text dark:text-dark-text">
+      {{ $t('auth.register-page.login-message') }}
+      <span class="text-secondary font-bold">
+        <NuxtLink to="/auth/login">
+          {{ $t('auth.common.login') }}
+        </NuxtLink>
+      </span>
+    </div>
   </div>
 </template>
